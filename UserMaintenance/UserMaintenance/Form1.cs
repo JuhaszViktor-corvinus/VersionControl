4444342cbc54f9137e4ad1f6ea8bcd4ad1bb149f
juhi priya <juhivi@gmail.com>
2020-09-27 11:41:49 +0200

    6os feladat uj userpeldany

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -10,14 +10,26 @@ using System.Windows.Forms;
 
 namespace UserMaintenance
 {
+   
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             lblLastName.Text = Resource1.LastName; // label1
             lblFirstName.Text = Resource1.FirstName; // label2
             btnAdd.Text = Resource1.Add; // button1
+
+            listUsers.DataSource = users;
+            listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
+            var u = new User()
+            {
+                LastName = txtLastName.Text,
+                FirstName = txtLastName.Text
+            };
+            users.Add(u);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -26,6 +38,11 @@ namespace UserMaintenance
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
