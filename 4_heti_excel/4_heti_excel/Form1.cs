0dc44f378ee3d9f5f99df2cf54dec47aa3f6e392
juhi priya <juhivi@gmail.com>
2020-09-30 13:06:22 +0200

    6-7-feladat

--- 4_heti_excel/4_heti_excel/Form1.cs
+++ 4_heti_excel/4_heti_excel/Form1.cs
@@ -20,11 +20,97 @@ namespace _4_heti_excel
         {
             InitializeComponent();
             LoadData();
-                        
+            Createtable();
+            Excel.Application xlApp; // A Microsoft Excel alkalmazás
+            Excel.Workbook xlWB; // A létrehozott munkafüzet
+            Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
         }
         private void LoadData()
         {
             Flats = context.Flat.ToList();
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+        private void Createtable()
+        {
+            
+            string[] headers = new string[] {
+                 "Kód",
+                 "Eladó",
+                 "Oldal",
+                 "Kerület",
+                 "Lift",
+                 "Szobák száma",
+                 "Alapterület (m2)",
+                 "Ár (mFt)",
+                 "Négyzetméter ár (Ft/m2)"};
+            object[,] values = new object[Flats.Count, headers.Length];
+            for (int i = 0; i+1 < headers.Length; i++)
+            {
+                i=xlSheet.Cells[1, 1] = headers[0];
+            }
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                if (f.Elevator == true)
+                {
+                    Text = "Van";
+                }
+                else
+                {
+                    Text = "Nincs";
+                }
+                values[counter, 8] = "";
+                counter++;
+            }
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
         }
     }
 }
