d601436a613999601227788524cb56f003803f7f
juhi priya <juhivi@gmail.com>
2020-10-15 15:14:43 +0200

    2_feladat

---
+++ tasks_week06/tasks_week06/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tasks_week06.MnbServiceReference;
+
+namespace tasks_week06
+{
+    public partial class Form1 : Form
+    {
+        var mnbService = new MNBArfolyamServiceSoapClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
