7b3f733d3d2ad07583179756268032e9b1995aeb
juhi priya <juhivi@gmail.com>
2020-10-25 11:58:09 +0100

    2-3_feladat

---
+++ Mikroszimulaci(7_het)/Mikroszimulaci(7_het)/Entities/Gender.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimulaci_7_het_.Entities
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+    
+    public class Person
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public int NbrOfChildren { get; set; }
+        public bool IsAlive { get; set; }
+
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double Birthprobability { get; set; }
+               
+    }
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double Deathprobability { get; set; }
+
+    }
+
+}
