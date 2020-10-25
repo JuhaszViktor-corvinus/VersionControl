3d85b288842714a2fcf4f1d6fde5ae0831b40a1d
juhi priya <juhivi@gmail.com>
2020-10-25 12:20:50 +0100

    5_feladat

--- Mikroszimulaci(7_het)/Mikroszimulaci(7_het)/Form1.cs
+++ Mikroszimulaci(7_het)/Mikroszimulaci(7_het)/Form1.cs
@@ -14,6 +14,7 @@ namespace Mikroszimulaci_7_het_
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random(1234)
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
