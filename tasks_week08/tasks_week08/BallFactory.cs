c47d09b207451287cd31ec6df9158f38558c16e7
juhi priya <juhivi@gmail.com>
2020-11-13 10:20:26 +0100

    2-3_feladat

---
+++ tasks_week08/tasks_week08/BallFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tasks_week08
+{
+    class BallFactory
+    {
+        public class BallFactor
+        {
+            public Ball CreateNew()
+            {
+                return new Ball();
+            }
+        }
+    }
+}
