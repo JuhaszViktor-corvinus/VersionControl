dcc1c76e31087303eca74416f11073f8852a13dd
juhi priya <juhivi@gmail.com>
2020-11-22 10:15:41 +0100

    5_feladat

---
+++ UnitTestExample/UnitTestExamle.Test/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExamle.Test
+{
+    class AccountController
+    {
+        // Arrange
+        var accountController = new AccountController();
+
+        // Act
+        var actualResult = accountController.ValidateEmail(email);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
