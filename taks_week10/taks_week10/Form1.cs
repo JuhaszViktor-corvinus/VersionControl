60dba5cd1fd6ef8ed562a2fb2e958dff47004fc8
juhi priya <juhivi@gmail.com>
2020-11-29 13:09:14 +0100

    7_feladat

--- taks_week10/taks_week10/Form1.cs
+++ taks_week10/taks_week10/Form1.cs
@@ -14,8 +14,9 @@ namespace taks_week10
     
         public partial class Form1 : Form
         {
-            GameController gc = new GameController();
-            GameArea ga;
+        Brain winnerBrain = null;
+        GameController gc = new GameController();
+        GameArea ga;
         private object label1;
 
         public Form1()
@@ -48,6 +49,7 @@ namespace taks_week10
                     "{0}. generáció",
                     generation);
                 gc.ResetCurrentLevel();
+
                 foreach (var p in topPerformers)
                 {
                     var b = p.Brain.Clone();
@@ -60,6 +62,15 @@ namespace taks_week10
                         gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
                     else
                         gc.AddPlayer(b.Mutate());
+                }
+                var winners = from p in topPerformers
+                              where p.IsWinner
+                              select p;
+                if (winners.Count() > 0)
+                {
+                    winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                    gc.GameOver -= Gc_GameOver;
+                    return;
                 }
                 gc.Start();
             }
