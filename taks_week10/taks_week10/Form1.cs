d7f98c565feb417fa4b344e803eec7c7b28588f8
juhi priya <juhivi@gmail.com>
2020-11-29 13:02:53 +0100

    2-3-4-5_feladat

--- taks_week10/taks_week10/Form1.cs
+++ taks_week10/taks_week10/Form1.cs
@@ -7,14 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace taks_week10
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
+    
+        public partial class Form1 : Form
         {
-            InitializeComponent();
+            GameController gc = new GameController();
+            GameArea ga;
+
+            public Form1()
+            {
+                ga = gc.ActivateDisplay();
+                this.Controls.Add(ga);
+                gc.AddPlayer();
+                gc.Start(true);
+
+                int populationSize = 100;
+                int nbrOfSteps = 10;
+                int nbrOfStepsIncrement = 10;
+                int generation = 1;
+                gc.GameOver += Gc_GameOver;
+
+                for (int i = 0; i < populationSize; i++)
+                {
+                    gc.AddPlayer(nbrOfSteps);
+                }
+                gc.Start();
+                var playerList = from p in gc.GetCurrentPlayers()
+                                 orderby p.GetFitness() descending
+                                 select p;
+                var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            }
+            private void Gc_GameOver(object sender)
+            {
+                generation++;
+                label1.Text = string.Format(
+                    "{0}. generáció",
+                    generation);
+            }
         }
     }
 }
